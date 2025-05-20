5fc61b81927e17572ab5b2a4a800e0c439b767c6
lsalehar <lsalehar@users.noreply.github.com>
2025-05-20 14:55:24 +0200

    Removed redundant definitions of pages.

--- aero_data/aero_data.py
+++ aero_data/aero_data.py
@@ -1,6 +1,5 @@
 import reflex as rx
+
 from aero_data.pages import index, status
 
 app = rx.App(theme=rx.theme(accent_color="blue", gray_color="slate", color_mode="inherit"))  # type: ignore
-app.add_page(index)
-app.add_page(status)
