d009bd441c1aded6a8fa855671675a1bee5d8fc6
lsalehar <lsalehar@users.noreply.github.com>
2025-08-16 11:55:53 +0200

    Migration to uv and bumping dependancies.

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import toml
 
 def get_version():
     pyproject_data = toml.load("pyproject.toml")
-    return pyproject_data["tool"]["poetry"]["version"]
+    return pyproject_data["project"]["version"]
 
 
-config = rx.Config(app_name="aero_data", version=get_version(), show_built_with_reflex=False)
+config = rx.Config(app_name="aero_data", version=get_version(), show_built_with_reflex=False)  # pyright: ignore[reportCallIssue]
